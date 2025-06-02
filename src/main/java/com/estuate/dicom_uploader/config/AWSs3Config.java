26b9d2c23781ac7ecc5c7e54d252b296e1cc1df6
Ayan Pal <APal@ESTUATE.COM>
2025-06-02 12:26:58 +0530

    updated yml

--- src/main/java/com/estuate/dicom_uploader/config/AWSs3Config.java
+++ src/main/java/com/estuate/dicom_uploader/config/AWSs3Config.java
@@ -3,44 +3,53 @@ package com.estuate.dicom_uploader.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.sts.StsClient;
+import software.amazon.awssdk.services.sts.auth.StsAssumeRoleCredentialsProvider;
+import software.amazon.awssdk.services.sts.model.AssumeRoleRequest;
 
 @Configuration
 public class AWSs3Config {
 
-
-    @Value("${aws.access-key}")
-    private String accessKey;
-
-    @Value("${aws.secret-key}")
-    private String secretKey;
+    @Value("${aws.s3.roleArn}")
+    private String roleArn;
 
     @Value("${aws.region}")
     private String region;
 
-    @Bean
-    public S3Presigner s3Presigner() {
-        return S3Presigner.builder()
-                .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)
-                ))
+    private AwsCredentialsProvider assumedRoleProvider() {
+        return StsAssumeRoleCredentialsProvider.builder()
+                .stsClient(StsClient.builder()
+                        .region(Region.of(region))
+                        .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                        .build())
+                .refreshRequest(AssumeRoleRequest.builder()
+                        .roleArn(roleArn)
+                        .roleSessionName("springboot-session")
+                        .durationSeconds(3600)
+                        .build())
+                .asyncCredentialUpdateEnabled(true)
                 .build();
     }
+
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
                 .region(Region.of(region))
-                .credentialsProvider(StaticCredentialsProvider.create(
-                        AwsBasicCredentials.create(accessKey, secretKey)
-                ))
+                .credentialsProvider(assumedRoleProvider())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .region(Region.of(region))
+                .credentialsProvider(assumedRoleProvider())
                 .build();
     }
 }
-
-
